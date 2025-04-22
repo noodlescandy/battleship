cac8bbf642b38f0ba1193d63e50f6c3a74813f02
noodlescandy <conquerorix@gmail.com>
2025-04-21 21:08:41 -0400

    implemented start command functionality

--- server/server.js
+++ server/server.js
@@ -4,14 +4,14 @@ const portNo = 9576;
 const wss = new WebSocket.Server({ port: portNo });
 
 connections = [];
-const games = {}; // dict with game codes as key and list of players as value
+games = {}; // dict with game codes as key and list of players as value
 
 wss.on('connection', (ws) => {
     // inital connection
     connections.push(ws);
     console.log('Client connected. Total:', connections.length);
     state = "init";
-    server_code = -1;
+    lobby = -1;
     
     // handle messages and states
     ws.on('message', (message) => {
@@ -21,15 +21,22 @@ wss.on('connection', (ws) => {
             case "init": // server setup state (not in lobby)
                 // based on message, should be about either starting a game or joining a game
                 if (messageData === "start"){
-                    // generate code for lobby, making sure it isn't being used already in games
-                    // create new entry in games with the code as the key and the value as a list with the ws as the first index
-                    // send to client the code of the game, for the client to share
-                    ws.send(JSON.stringify("0000"));
+                    if (lobby != -1){ // already in lobby
+                        ws.send(JSON.stringify(lobby.toString()));
+                        break;
+                    }
+                    do {
+                        lobby = Math.floor(Math.random() * 10000);
+                    } while (lobby in games)
+                    games[lobby] = [ws];
+                    console.log("New Game", lobby, "created.")
+                    ws.send(JSON.stringify(lobby.toString()));
                 }
                 else{
                     content = messageData.split(" ");
                     if (content[0] === "join"){
                         code = content[1];
+                        // TODO
                         // check for code in games
                         // if code not found, return error
                         ws.send(JSON.stringify("Error: code not found"));
@@ -46,6 +53,7 @@ wss.on('connection', (ws) => {
                     }
                 }
                 break;
+            // TODO - other states -- setup (ship placement), gameTurn, gameWait (turns)
             default:
                 // unknown state/ not implemented
                 if (ws.readyState === WebSocket.OPEN) {
@@ -59,7 +67,8 @@ wss.on('connection', (ws) => {
         // end game, tell other client if connected
         connections = connections.filter(connection => connection !== ws);
         console.log('Connection closed. Total:', connections.length);
-        if(server_code != -1){
+        if(lobby != -1){
+            // TODO
             // connected to a game lobby, close the lobby for all players.
             console.log("closing game lobby");
             // get other ws listed in list that isn't this ws
